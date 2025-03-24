7ab4c840219345a27d10870797edccacaa15fc35
Sainath57 <sainath22042002@gmail.com>
2025-03-24 16:18:47 -0400

    updated home section

--- src/components/About.js
+++ src/components/About.js
@@ -8,6 +8,16 @@ const About = () => {
             <p>
                 Hi, I'm Sainath. I'm a student passionate about building web applications using ReactJS.
             </p>
+            <div className="intro">
+                <p className="intro1"><h1>Welcome to Sainath's Portfolio !</h1></p>
+                <p className="intro2">Sainath Talakanti, commonly known as Sainath is Master's Student majoring in
+                    Computer Science. Just like you, he is very passionate in what he does and waiting for the
+                    opportunities by ringing bells.</p>
+                <br/>
+                <p className="intro2">Counting them on fingers, Sainath has ennumerable skills. Take it about his
+                    behaviour, problem solving, knowledge, curiosity, communication , technical especially and many
+                    more.</p>
+            </div>
         </section>
     );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,44 @@ import "./Home.css"
 function Home() {
     return (
         <div className="Home">
-            <div className="profilepic-container">
-                <img className="profilePic" src="/images/profile-pic1.JPG" alt="Sainath Profile Picture"/>
+            <div className="home-container">
+                <div className="profilepic-container">
+                    <img className="profilePic" src="/images/profile-pic1.JPG" alt="Sainath Profile Picture"/>
+                </div>
+                <div className="intro">
+                    <p className="intro1">Sainath R. Talakanti<sub>(SRT)</sub></p>
+                    <br/>
+                    <p className="intro2">Software Engineer | Java Full Stack Developer | DevOps Engineer</p>
+                    <br/>
+                    <p className="intro3">Pleasure to have your here!</p>
+                    <br/>
+                    <div className="platform-icons">
+                        <a className="resume-icon">
+                            <img width="100" height="100" src="https://img.icons8.com/clouds/100/resume.png"
+                                 alt="resume"/>
+                        </a>
+                        <a className="linkedin-icon" href={"https://linkedin.com/in/sainathtalakanti//"}
+                           alt={"sainathtalakanti}"} target="_blank" rel="noopener noreferrer">
+                            <img width="96" height="96" src="https://img.icons8.com/color/96/linkedin.png"
+                                 alt="linkedin"/>
+                        </a>
+                        <a className="gmail-icon" href={"mailto:sainathtalakanti09@gmail.com"}
+                           alt={"sainathtalakanti09@gmail.com"} target="_blank" rel="noopener noreferrer">
+                            <img width="96" height="96" src="https://img.icons8.com/color/96/gmail--v1.png"
+                                 alt="gmail--v1"/>
+                        </a>
+                        <a className="github-icon" href={"https://github.com/Sainath57"} alt={"Sainath57"}
+                           target="_blank" rel="noopener noreferrer">
+                            <img width="96" height="96" src="https://img.icons8.com/color/96/github--v1.png"
+                                 alt="github--v1"/>
+                        </a>
+                        <a className="X-icon" href={"https://x.com/sainathtreddy"} alt={"sainathtreddy"} target="_blank"
+                           rel="noopener noreferrer">
+                            <img width="96" height="96" src="https://img.icons8.com/color/96/twitterx--v1.png"
+                                 alt="twitterx--v1"/>
+                        </a>
+                    </div>
+                </div>
             </div>
             <div className="ps">
                 <p className="ps1">Thank you for visiting my Portfolio! ❤️</p>
